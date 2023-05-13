30619024745358fbffefdd51c98084df434764ae
lysia <lysia@evnts.com.br>
2023-05-13 13:23:02 -0300

    style: :lipstick: adding react toast in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components'
 import { Routes } from './routes'
 import { Theme } from './styles/theme';
 import { GlobalStyle } from './styles/global'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   return (
     <ThemeProvider theme={Theme}>
       <Routes />
       <GlobalStyle/>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        gutter={8}
+        containerClassName=""
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: '',
+          duration: 5000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+
+          // Default options for specific types
+          success: {
+            duration: 3000,
+            theme: {
+              primary: 'green',
+              secondary: 'black',
+            },
+          },
+        }}
+      />
     </ThemeProvider>
   )
 }
