440cb53acb49e030a61cd4b6abbd1b676cb48410
lysia <lysia@evnts.com.br>
2023-05-13 13:28:07 -0300

    feat: :zap: using the hook userUser functions

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -3,16 +3,19 @@ import { LoginForm } from '../../components/LoginForm'
 import { Layout } from '../../components/Layout'
 
 import { Container, ImageContent, Content  } from './styles'
+import { useUser } from '../../hooks/useUser'
 
 export const Login = () => {
+
+  const { handleOnChange, onSubmit } = useUser()
 
   return(
     <Layout>
       <Container>
-          {/* <ImageContent /> */}
+          <ImageContent />
         <Content>
           <h1>Olá novamente</h1>
-          <LoginForm/>
+          <LoginForm onChange={handleOnChange} onSubmit={onSubmit}/>
         </Content>
       </Container>
     </Layout>
