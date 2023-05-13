b9b4b6b106de17f222a6ae13f3ac88ab6ec211b2
lysia <lysia@evnts.com.br>
2023-05-13 13:24:42 -0300

    feat: :sparkles: implementing the login form

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,22 +1,34 @@
+import PropTypes from 'prop-types'
+
+import { SelectButton } from 'primereact/selectbutton'
 import { InputText } from 'primereact/inputtext'
 import { Password } from 'primereact/password'
 import { Button } from 'primereact/button'
 
 import { Form } from './styles'
 
-export const LoginForm = () => {
+export const LoginForm = ({ onChange, onSubmit, loading }) => {
   return (
-    <Form>
+    <Form onSubmit={onSubmit}>
       <span className="p-float-label">
-        <InputText type='email' id="login"/>
+        <InputText type='email' id="login" onChange={(event) => onChange({ id: 'login', value: event.target.value })} required />
         <label htmlFor="login">Login</label>
       </span>
       <span className="p-float-label">
-        <Password id="password"/>
-        <label htmlFor="password">Password</label>
+        <Password id="password" onChange={(event) => onChange({ id: 'password', value: event.target.value })} required />
+        <label htmlFor="password">Senha</label>
       </span>
-      <Button label="Login" className="p-button-success" loading={false}/>
+      <div className="card flex justify-content-center">
+        <SelectButton value={'type'} onChange={(event) => onChange({ id: 'type', value: event.value })} options={['cnpj', 'cpf']} required />
+      </div>
+      <Button label="Login" className="p-button-success" loading={loading} />
       <p>Não tem uma conta? registre-se</p>
     </Form>
   )
+}
+
+LoginForm.propTypes = {
+  onChange: PropTypes.func,
+  onSubmit: PropTypes.func,
+  loading: PropTypes.bool
 }
--- src/components/LoginForm/styles.js
+++ src/components/LoginForm/styles.js
@@ -4,6 +4,19 @@ export const Form = styled.form`
   display: flex;
   flex-direction: column;
 
+  align-items: center;
+
   padding: 20px;
   gap: 30px;
+
+  .card {
+    width: 100%;
+    .p-button {
+      width: 50%;
+    }
+  }
+
+  .p-button {
+    width: 100%;
+  }
 `
