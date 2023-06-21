2163b4ca633252bab57c8f7b509351ec616871ba
lysia <lysia@evnts.com.br>
2023-06-20 23:03:28 -0300

    feat: :zap: adding constant

--- src/constants/form.js
+++ src/constants/form.js
@@ -1,5 +1,6 @@
 export const FORM = {
   NAME: 'Nome',
   LAST_NAME: 'Sobrenome',
-  REGISTER_BUTTON: 'Cadastrar! 🧡'
+  REGISTER_BUTTON: 'Cadastrar! 🧡',
+  ENTRY_LOCATION: 'Selecione sua localização'
 }
